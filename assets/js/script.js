c6088c1ce713bbc424ce0223bf8a21e0fcddb914
Kareem Rizk <kareem.magdy5@gmail.com>
2021-12-03 22:06:39 -0500

    added new event listner for the form element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
-var buttonEl = document.querySelector("#save-task"); 
+// var buttonEl = document.querySelector("#save-task"); 
+var formEl = document.querySelector("#task-form");
+
 var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var createTaskHandler = function() { 
+var createTaskHandler = function(event) { 
+    event.preventDefault();
+
   var listItemEl = document.createElement("li"); 
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task."; 
-  tasksToDoEl.appendChild(listItemEl); 
+  tasksToDoEl.appendChild(listItemEl);
   }; 
+//the "submit" is the event lisnterer and the createTaskHandler is the event handler
+  formEl.addEventListener("submit", createTaskHandler);
 
-buttonEl.addEventListener("click", createTaskHandler);
+// buttonEl.addEventListener("click", createTaskHandler);
 
 
 
